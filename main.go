8c2f5e1eefd28ca9a1cd67e7f87a4de46ad4e575
Ezep02 <pereyraezequiel15617866@outlook.es>
2025-05-18 17:45:42 -0300

    new sse architecture, and cors configuration

--- main.go
+++ main.go
@@ -1,20 +1,137 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"ssr-go/models"
-	"sync"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/cors"
 )
 
-var (
-	clients  = make(map[chan []byte]bool)
-	clientMu sync.Mutex
-)
+type BroadCaster struct {
+	// Channel to broadcast messages
+	broadcast chan []byte
+
+	// Channel to accept new connections
+	newConnection chan chan []byte
+
+	// Channel to close connections
+	closeConnection chan chan []byte
+
+	// Connections
+	connections map[chan []byte]int32
+}
+
+func (b *BroadCaster) Listen() {
+
+	// ticker := time.Tick(6 * time.Second)
+	var seq int32 = 0
+	for {
+
+		select {
+		// broadcast message to all connections
+		case message := <-b.broadcast:
+			for connection := range b.connections {
+				connection <- message
+			}
+		// add a new connection
+		case connection := <-b.newConnection:
+			b.connections[connection] = seq
+			seq++
+			log.Println("[NEW CONNECTION]")
+		// close connection
+		case connection := <-b.closeConnection:
+			delete(b.connections, connection)
+			log.Println("[CLIENT HAS GONE]")
+		}
+	}
+}
+
+func StartPinging(b *BroadCaster) {
+
+	ticker := time.NewTicker(3 * time.Second)
+	quit := make(chan struct{})
+
+	go func() {
+
+		for {
+			select {
+			case <-ticker.C:
+				currentTime := strconv.FormatInt(time.Now().Unix(), 10)
+				b.broadcast <- []byte(currentTime)
+
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+
+		}
+
+	}()
+
+}
+
+type Server struct {
+	broadcaster *BroadCaster
+}
+
+func NewBroadcaster() *BroadCaster {
+	return &BroadCaster{
+		broadcast:       make(chan []byte),
+		newConnection:   make(chan chan []byte),
+		closeConnection: make(chan chan []byte),
+		connections:     make(map[chan []byte]int32),
+	}
+}
+
+func NewServer() *Server {
+	broadCaster := NewBroadcaster()
+	server := &Server{
+		broadCaster,
+	}
+
+	go broadCaster.Listen()
+	return server
+}
+
+func setHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func (server *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	fluser, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	setHeaders(w)
+
+	clientConnection := make(chan []byte)
+	server.broadcaster.newConnection <- clientConnection
+
+	defer func() {
+		server.broadcaster.closeConnection <- clientConnection
+	}()
+
+	isConnectionClose := r.Context().Done()
+
+	go func() {
+		<-isConnectionClose
+		server.broadcaster.closeConnection <- clientConnection
+	}()
+
+	for {
+		fmt.Fprintf(w, "data: %s\n\n", <-clientConnection)
+		fluser.Flush()
+	}
+}
 
 func main() {
 	// Mux base
@@ -30,78 +147,12 @@ func main() {
 		MaxAge:           300,
 	}).Handler(mux)
 
-	// Rutas
-	mux.HandleFunc("/events", EventsHandler)
-	mux.HandleFunc("/orders", OrdersHandler)
+	server := NewServer()
 
-	// Inicia servidor con CORS
-	log.Println("Server is working on port :9090")
-	if err := http.ListenAndServe(":9090", handler); err != nil {
-		log.Fatalf("Unable to start server %s", err.Error())
-	}
+	StartPinging(server.broadcaster)
 
-}
+	mux.HandleFunc("/event", server.ServeHTTP)
 
-func OrdersHandler(w http.ResponseWriter, r *http.Request) {
-	var placed_order models.Order
-
-	if err := json.NewDecoder(r.Body).Decode(&placed_order); err != nil {
-		http.Error(w, "Error recibiendo los datos", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-	log.Println("[OrderHandler] Orden recibida:", placed_order)
-
-	msgBytes, err := json.Marshal(placed_order)
-	if err != nil {
-		http.Error(w, "Error serializando orden", http.StatusInternalServerError)
-		return
-	}
-
-	for ch := range clients {
-		select {
-		case ch <- msgBytes:
-		default:
-			log.Println("[WARNING] cliente no responde")
-		}
-	}
-
-	w.WriteHeader(http.StatusAccepted)
-	fmt.Fprintln(w, "Orden recibida OK")
-}
-
-func EventsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		http.Error(w, "Streaming no soportado", http.StatusInternalServerError)
-		return
-	}
-
-	messageChan := make(chan []byte)
-	clientMu.Lock()
-	clients[messageChan] = true
-	clientMu.Unlock()
-
-	defer func() {
-		clientMu.Lock()
-		delete(clients, messageChan)
-		clientMu.Unlock()
-		close(messageChan)
-	}()
-
-	// lectura de eventos para un cliente especifico
-	for {
-		msg, ok := <-messageChan
-		if !ok {
-			break
-		}
-		fmt.Fprintf(w, "data: %s\n\n", msg)
-		flusher.Flush()
-	}
-
+	log.Println("Starting")
+	log.Println("Error: ", http.ListenAndServe("localhost:9090", handler))
 }
